b35a9f8d6a63497d3652f1911467b1f343b6d444
z1pf1le <total.zip.file@gmail.com>
2023-11-04 11:55:12 +0300

    Initial commit

---
+++ src/ReadObject.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ReadObject{
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fis = new FileInputStream("people.bin");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Person person1 = (Person)ois.readObject();
+            Person person2 = (Person)ois.readObject();
+
+            System.out.println(person1);
+            System.out.println(person2);
+
+            ois.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
