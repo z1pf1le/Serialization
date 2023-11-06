ed4595792cfdf4d0d65ea265cbffe13cb2995e83
z1pf1le <total.zip.file@gmail.com>
2023-11-06 09:58:44 +0300

    рабочий короткий пример сериализации с дополнениями

--- src/userExample/Main.java
+++ src/userExample/Main.java
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=vz6nyRXCUs0
 package userExample;
 
 import java.io.*;
@@ -17,10 +18,26 @@ public class Main{
         User newUser = (User) objectInputStream.readObject();
         objectInputStream.close();
         System.out.println(newUser.lifeLevel);
+
+        Pupil pupil = new Pupil();
+        pupil.lifeLevel = 90;
+        pupil.grade = 4;
+        FileOutputStream fileOutputStream2 = new FileOutputStream("/home/andrei/tempFile");
+        ObjectOutputStream objectOutputStream2 = new ObjectOutputStream(fileOutputStream2);
+        objectOutputStream2.writeObject(pupil);
+        objectOutputStream2.close();
+        FileInputStream fileInputStream2 = new FileInputStream("/home/andrei/tempFile");
+        ObjectInputStream objectInputStream2 = new ObjectInputStream(fileInputStream2);
+        Pupil newPupil = (Pupil) objectInputStream2.readObject();
+        objectInputStream2.close();
+        System.out.println(newPupil.grade);
+        System.out.println(newPupil.lifeLevel);
     }
 }
-
 class User implements Serializable{
     int lifeLevel;
+}
+class Pupil extends User {
+    int grade;
 }
 
